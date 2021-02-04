63a64433d29042c0c931f17a24057147831b469b
asaqui94 <arpitasaqui@gmail.com>
2021-02-03 23:54:12 -0500

    day-3 user authentication

--- car_api/models.py
+++ car_api/models.py
@@ -1,9 +1,16 @@
-from car_api import app, db
+from car_api import app, db, login_manager
 import uuid
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key=True)
     first_name = db.Column(db.String(150), nullable=True, default='')
     last_name = db.Column(db.String(150), nullable=True, default='')
@@ -12,13 +19,19 @@ class User(db.Model):
     g_auth_verify = db.Column(db.Boolean, default=False)
     token = db.Column(db.String, default='')
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    car = db.relationship('Car', backref = 'owner', lazy = True)
 
-    def __init__(self,email,first_name='',last_name='',id='',password=''):
+    def __init__(self,email,first_name='',last_name='',id='',password='', token='',g_auth_verify=False):
         self.id = self.set_id()
         self.first_name = first_name
         self.last_name = last_name
         self.password = self.set_password(password)
         self.email = email
+        self.token = self.set_token(24)
+        self.g_auth_verify = g_auth_verify
+
+    def set_token(self, length):
+        return secrets.token_hex(length)
 
     def set_id(self):
         return str(uuid.uuid4())
@@ -30,3 +43,29 @@ class User(db.Model):
         def __repr__(self):
             return f'User {self.email} has been added to the database.'
 
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    price = db.Column(db.Integer)
+    model = db.Column(db.String(150))
+    year = db.Column(db.Integer)
+    user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self,name,price,model,year,user_id):
+        self.name = name
+        self.price = price
+        self.model = model
+        self.year = year
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f'The following car has been added: {self.year} {self.name} {self.model} which belongs to {self.user_id}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "model": self.model,
+            "year": self.year
+        }
